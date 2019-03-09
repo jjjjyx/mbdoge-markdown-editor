c278a92d6da832d55dd24f6ada63902b736157e3
jjjjyx <jyx@rpgame.net>
2019-03-09 16:01:16 +0800

    fix: 编译环境调整，构建目标为lib；调整项目代码属性

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import MbdogeEditor from './mbdoge-editor'
+
+module.exports = {
+    install (Vue) {
+        Vue.component(MbdogeEditor.name, MbdogeEditor)
+    }
+}
--- src/mbdoge-editor.vue
+++ src/mbdoge-editor.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import './assets/scss/index.scss'
+
 export default {
     name: 'mbdoge-editor'
 }
